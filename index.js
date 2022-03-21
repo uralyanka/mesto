923624b337b4e6d42b608c142e941963c9d6cd05
uralyanka <94884939+uralyanka@users.noreply.github.com>
2022-03-21 19:33:30 +0500

    hf 21/03 2.0

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let editUserButton = document.querySelector('.profile__user-edit-button');
 let closeEditUserButton = document.querySelector('.popup__close-btn_user-edit');
-let popupEditUser = document.querySelector('.popup_user-edit');
 
-let formElement = document.querySelector('.popup__form_user-edit');
-let nameInput = document.querySelector('.popup__input_type_user-name');
-let bioInput = document.querySelector('.popup__input_type_user-bio');
+let popupEditUser = document.querySelector('.popup_type_user-edit');
+let formElement = popupEditUser.querySelector('.popup__form');
+
+let nameInput = formElement.querySelector('#user-name');
+let bioInput = formElement.querySelector('#user-bio');
 
 let userName = document.querySelector('.profile__user-name');
 let userBio = document.querySelector('.profile__user-bio');
@@ -34,4 +35,3 @@ editUserButton.addEventListener('click', onPopup);
 closeEditUserButton.addEventListener('click', offPopup);
 
 formElement.addEventListener('submit', formSubmitHandler); 
-
