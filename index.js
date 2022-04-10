0e2b1be83be60b109e701da04f97bb35fe2123d0
uralyanka <94884939+uralyanka@users.noreply.github.com>
2022-04-10 20:06:34 +0500

    feature js pr5 10/04

--- index.js
+++ index.js
@@ -6,37 +6,54 @@ const mestoTemplate = document.querySelector('#mesto-template').content;
 const initialCards = [
   {
     name: 'Луна',
-    link: 'https://cdn.pixabay.com/photo/2018/02/12/21/03/planet-3149121_960_720.jpg'
+    link: 'https://cdn.pixabay.com/photo/2018/02/12/21/03/planet-3149121_960_720.jpg',
+    alt: 'Лунная Луна'
   },
   {
     name: 'Моя планета Мелмак',
-    link: 'https://avatars.mds.yandex.net/get-zen_doc/1821029/pub_5eafa60434cbba0565c6b3f2_5eafdcae0ab5b766d08574d5/scale_1200'
+    link: 'https://avatars.mds.yandex.net/get-zen_doc/1821029/pub_5eafa60434cbba0565c6b3f2_5eafdcae0ab5b766d08574d5/scale_1200',
+    alt: 'Мелмаковый Мелмак'
   },
   {
     name: 'Планета Венера',
-    link: 'https://cdn.pixabay.com/photo/2016/04/25/01/30/venus-1351056_960_720.png'
+    link: 'https://cdn.pixabay.com/photo/2016/04/25/01/30/venus-1351056_960_720.png',
+    alt: 'Венерная Венера'
   },
   {
     name: 'Планета Марс',
-    link: 'https://cdn.pixabay.com/photo/2017/02/09/12/02/mars-2051747_960_720.png'
+    link: 'https://cdn.pixabay.com/photo/2017/02/09/12/02/mars-2051747_960_720.png',
+    alt: 'Марсовый Марс'
   },
   {
     name: 'Меркурий',
-    link: 'https://cdn.pixabay.com/photo/2020/09/08/23/58/mercury-5556108_960_720.jpg'
+    link: 'https://cdn.pixabay.com/photo/2020/09/08/23/58/mercury-5556108_960_720.jpg',
+    alt: 'Меркурьевий Меркурий'
   },
   {
     name: 'Юпитер',
-    link: 'https://cdn.pixabay.com/photo/2012/01/09/10/41/space-probe-11616_960_720.jpg'
+    link: 'https://cdn.pixabay.com/photo/2012/01/09/10/41/space-probe-11616_960_720.jpg',
+    alt: 'Юпитерный Юпитер'
   }
 ];
   
 initialCards.forEach(function (element) {
   const mestoElement = mestoTemplate.cloneNode(true);
     mestoElement.querySelector('.elements__item-image').src = element.link;
+    mestoElement.querySelector('.elements__item-image').alt = element.alt;
     mestoElement.querySelector('.elements__item-name').textContent = element.name;
        
     elementsList.append(mestoElement)
 });
+
+/*Функция открытия попапов*/
+function popupOpen(popup) {
+  popup.classList.add('popup_opened');
+}
+
+/*Функция закрытия попапов*/
+function popupClose(popup) {
+  popup.classList.remove('popup_opened');
+}
 
 /*КНОПКА КАРАНДАШ*/
 let editUserButton = document.querySelector('.profile__user-edit-button');
@@ -52,17 +69,18 @@ let userName = document.querySelector('.profile__user-name');
 let userBio = document.querySelector('.profile__user-bio');
 
 function onPopupEdit() {
-    popupEditUser.classList.add('popup_opened');
-    nameInput.value = userName.innerText;
-    bioInput.value = userBio.innerText;
+  popupOpen(popupEditUser);
+  nameInput.value = userName.innerText;
+  bioInput.value = userBio.innerText;
 }
 
 function offPopupEdit() {
-    popupEditUser.classList.remove('popup_opened');
-    nameInput.value = '';
-    bioInput.value = '';
-}
-/*КНОПКА СОХРАНИТЬ*/
+  popupClose(popupEditUser);
+  nameInput.value = '';
+  bioInput.value = '';
+} 
+
+/*КНОПКА СОХРАНИТЬ В КАРАНДАШЕ*/
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
@@ -76,9 +94,10 @@ editUserButton.addEventListener('click', onPopupEdit);
 closeEditUserButton.addEventListener('click', offPopupEdit);
 formElement.addEventListener('submit', formSubmitHandler); 
 
+
 /*КНОПКА ЛАЙК*/
 const likeMesto = Array.from(document.querySelectorAll('.elements__item-like'));
-likeMesto.forEach((button, index) => {
+likeMesto.forEach((button) => {
   btnLikeListener(button);
 });
 
@@ -87,10 +106,14 @@ function btnLikeListener(button) {
     button.classList.toggle('elements__item-like_active');
   });
 }
+/*
+document.querySelector('.elements__item-like').addEventListener('click', function (evt) {
+  evt.target.classList.toggle('elements__item-like_active')
+*/
 
 /*КНОПКА УДАЛИТЬ*/
 const deleteMesto = Array.from(document.querySelectorAll('.elements__trash-btn'));
-deleteMesto.forEach((button, index) => {
+deleteMesto.forEach((button) => {
   btnDeleteListener(button)
 });
 
@@ -100,7 +123,6 @@ function btnDeleteListener(button) {
     listItem.remove();
   });
 }
-
 
 /*КНОПКА +*/
 let addMestoButton = document.querySelector('.profile__add-button');
@@ -113,15 +135,15 @@ let mestoInput = formElementAdd.querySelector('.popup__input_type_mesto-name');
 let linkInput = formElementAdd.querySelector('.popup__input_type_mesto-link');
 
 function onPopupMesto() {
-    popupAddMesto.classList.add('popup_opened');
-    mestoInput.value = '';
-    linkInput.value = '';
+  popupOpen(popupAddMesto);
+  mestoInput.value = '';
+  linkInput.value = '';
 }
 
 function offPopupMesto() {
-    popupAddMesto.classList.remove('popup_opened');
-    mestoInput.value = '';
-    linkInput.value = '';
+  popupClose(popupAddMesto);
+  mestoInput.value = '';
+  linkInput.value = '';
 }
 
 /*КНОПКА СОЗДАТЬ В +*/
@@ -132,21 +154,92 @@ function addMesto(evt) {
 
   mestoElement.querySelector('.elements__item-name').textContent = mestoInput.value;
   mestoElement.querySelector('.elements__item-image').src = linkInput.value;
-  const likeBtn = mestoElement.querySelector('.elements__item-like')
-  const deleteBtn = mestoElement.querySelector('.elements__trash-btn')
+  const likeBtn = mestoElement.querySelector('.elements__item-like');
+  const deleteBtn = mestoElement.querySelector('.elements__trash-btn');
+  const imageViewBtn = mestoElement.querySelector('.elements__item-image');
   elementsList.prepend(mestoElement);
   btnLikeListener(likeBtn);
   btnDeleteListener(deleteBtn);
-  offPopupMesto ();
+  btnItemImageListener(imageViewBtn);
+  offPopupMesto();
 }
 
 addMestoButton.addEventListener('click', onPopupMesto);
 closeAddMestoButton.addEventListener('click', offPopupMesto);
-
 formElementAdd.addEventListener('submit', addMesto); 
 
+/*УВЕЛИЧЕНИЕ КАРТИНКИ*/
+const popupImageMesto = document.querySelector('.popup_type_image-mesto');
+const imageLink = document.querySelector('.popup__figure-image');
+const imageImage = document.querySelector('.popup__figure-caption');
+
+const itemsImage = Array.from(document.querySelectorAll('.elements__item-image'));
+itemsImage.forEach((button) => {
+  btnItemImageListener(button)
+});
+
+function btnItemImageListener(button) {
+  button.addEventListener('click', function (evt) {
+    popupOpen(popupImageMesto);
+    imageLink.src = evt.target.src;
+    imageLink.alt = evt.target.alt;
+    imageImage.textContent = document.querySelector('.elements__item-name').textContent;
+  });
+}
+
+const closeImageMestoButton = document.querySelector('.popup__close-btn_type_image-mesto');
+
+function offPopupImageMesto() {
+  popupClose(popupImageMesto);
+} 
+
+closeImageMestoButton.addEventListener('click', offPopupImageMesto);
 
 
+
+
+/*
+
+const itemImageMesto = document.querySelector('.elements__item-image');
+const popupImageMesto = document.querySelector('.popup_type_image-mesto');
+const imageLink = document.querySelector('.popup__figure-image');
+const imageImage = document.querySelector('.popup__figure-caption');
+
+const closeImageMestoButton = document.querySelector('.popup__close-btn_type_image-mesto');
+
+itemImageMesto.addEventListener('click', function (evt) {
+  popupOpen(popupImageMesto);
+  imageLink.src = evt.target.src;
+  imageLink.alt = evt.target.alt;
+  imageImage.textContent = document.querySelector('.elements__item-name').textContent;
+});
+
+function offPopupImageMesto() {
+  popupClose(popupImageMesto);
+} 
+
+closeImageMestoButton.addEventListener('click', offPopupImageMesto);
+
+/*
+let itemImageMesto = document.querySelector('.elements__item-image');
+let closeImageMestoButton = document.querySelector('.popup__close-btn_type_image-mesto');
+let popupImageMesto = document.querySelector('.popup_type_image-mesto');
+
+let imageMesto = document.querySelector('.elements__item-image');
+
+function onPopupMesto() {
+  popupOpen(popupImageMesto);
+  imageMesto.src = evt.target.src;
+  imageMesto.alt = evt.target.alt;
+  imageMesto.name = evt.target.name;
+}
+
+function offPopupMesto() {
+  popupClose(popupImageMesto);
+}
+
+itemImageMesto.addEventListener('click', onPopupMesto);
+closeImageMestoButton.addEventListener('click', offPopupMesto);
 
 
 /*
